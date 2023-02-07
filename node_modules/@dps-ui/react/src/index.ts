3de57480b188c52f90b1cbae6135122921f78720
Pedro Soares <ngrpedro22@gmail.com>
2023-02-07 13:59:22 -0300

    packages init config

--- node_modules/@dps-ui/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from "@dps-ui/tokens"
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,13 @@
+import { colors } from "@dps-ui/tokens"
+
+export function App() {
+  return (
+    <h1
+      style={{
+        color: colors.primary,
+      }}
+    >
+      Hello Word
+    </h1>
+  )
+}
