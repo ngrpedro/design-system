e9285a7ebc9cc18c846a0f1d900e597688de758c
Pedro Soares <ngrpedro22@gmail.com>
2023-02-08 13:09:24 -0300

    component:: box

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -1,25 +1,8 @@
 "use strict";
 var __defProp = Object.defineProperty;
-var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -37,63 +20,19 @@ var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: tru
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
-  Button: () => Button
+  Box: () => Box
 });
 module.exports = __toCommonJS(src_exports);
 
-// src/styles/index.ts
-var import_tokens = require("@dps-ui/tokens");
+// src/components/Box.tsx
 var import_react = require("@stitches/react");
-var {
-  styled,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-  createTheme,
-  config
-} = (0, import_react.createStitches)({
-  themeMap: __spreadProps(__spreadValues({}, import_react.defaultThemeMap), {
-    width: "space",
-    height: "space"
-  }),
-  theme: {
-    colors: import_tokens.colors,
-    fonts: import_tokens.fonts,
-    fontWeights: import_tokens.fontWeights,
-    fontSizes: import_tokens.fontSizes,
-    lineHeights: import_tokens.lineHeights,
-    radii: import_tokens.radii,
-    space: import_tokens.space
-  }
-});
-
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  fontWeight: "$bold",
-  backgroundColor: "$primary",
-  color: "$white",
+var Box = (0, import_react.styled)("div", {
+  padding: "$4",
   borderRadius: "$md",
-  height: "$10",
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4"
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
+  backgroundColor: "$gray800",
+  border: "1px solid $gray600"
 });
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  Button
+  Box
 });
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -1,83 +1,11 @@
-var __defProp = Object.defineProperty;
-var __defProps = Object.defineProperties;
-var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {}))
-    if (__hasOwnProp.call(b, prop))
-      __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop))
-        __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
-
-// src/styles/index.ts
-import {
-  colors,
-  fonts,
-  fontWeights,
-  fontSizes,
-  lineHeights,
-  radii,
-  space
-} from "@dps-ui/tokens";
-import { createStitches, defaultThemeMap } from "@stitches/react";
-var {
-  styled,
-  css,
-  globalCss,
-  keyframes,
-  getCssText,
-  theme,
-  createTheme,
-  config
-} = createStitches({
-  themeMap: __spreadProps(__spreadValues({}, defaultThemeMap), {
-    width: "space",
-    height: "space"
-  }),
-  theme: {
-    colors,
-    fonts,
-    fontWeights,
-    fontSizes,
-    lineHeights,
-    radii,
-    space
-  }
-});
-
-// src/index.tsx
-var Button = styled("button", {
-  fontFamily: "$default",
-  fontWeight: "$bold",
-  backgroundColor: "$primary",
-  color: "$white",
+// src/components/Box.tsx
+import { styled } from "@stitches/react";
+var Box = styled("div", {
+  padding: "$4",
   borderRadius: "$md",
-  height: "$10",
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: "$2 $4"
-      },
-      big: {
-        fontSize: 16,
-        padding: "$3 $6"
-      }
-    }
-  },
-  defaultVariants: {
-    size: "small"
-  }
+  backgroundColor: "$gray800",
+  border: "1px solid $gray600"
 });
 export {
-  Button
+  Box
 };
